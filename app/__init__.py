e32b85a5f5b66a3d5dc0b7298918e3d564d16af9
Mourouguesh COUNDJIDAPADAME <coundjim@pc5403t.esiee.fr>
2016-10-11 17:39:30 +0200

    change on init file for database-->correction

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
-init file (file app/__init__.py):
-
-
-app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
