80300e63c9ccfa68e5f10ee4a031c0afe6cd4831
Mourouguesh COUNDJIDAPADAME <coundjim@pc5403t.esiee.fr>
2016-10-17 14:07:43 +0200

    correction bdd

--- app/models.py
+++ app/models.py
@@ -5,17 +5,18 @@ class User(db.Model):
 	nom = db.Column(db.String(64), index=True, unique=True)
 	prenom = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
-	resp_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+	resp_id = db.relationship(db.Integer,db.ForeignKey('User.user_id'))
 	role = db.Column(db.Integer)
-
-class Vacances(db.Model):
-	date_debut = db.Column(db.DateTime)
-	date_fin = db.Column(db.DateTime)
-	nb_jour = db.Column(db.Integer)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-	status = db.Column(db.Integer)
-	
 
 def __repr__(self):
 	return '<User %r>' % (self.user_id)
+
+
+class Vacances(db.Model):
+	vacances_id = db.Column(db.Integer, primary_key=True)
+	date_debut = db.Column(db.DateTime)
+	date_fin = db.Column(db.DateTime)
+	nb_jour = db.Column(db.Integer)
+	user_id = db.Column(db.Integer,db.ForeignKey('User.user_id'))
+	status = db.Column(db.Integer)
 
