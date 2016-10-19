c632c8053e726a9d9a32217029a5e57ab585b551
Romain GILLE <giller@pc5403y.esiee.fr>
2016-10-19 10:11:18 +0200

    connection au ldap fonctionnel

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from ldap3 import Connection
-from ldap3 import Server, ALL
+from ldap3 import Server, ALL, AUTH_SIMPLE, STRATEGY_SYNC
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -12,13 +12,22 @@ db = SQLAlchemy(app)
 
 from app import views, models
 
-os.environ["HTTP_PROXY"] = "http://cache.esiee.fr:3128"
-os.environ["HTTPS_PROXY"] = "http://cache.esiee.fr:3128"
-server = Server('ldap.esiee.fr', use_ssl=True, get_info=ALL)
-conn = Connection(server, user="uid=foe,ou=Users,dc=esiee,dc=fr", password='plop')
+#os.environ["HTTP_PROXY"] = "http://cache.esiee.fr:3128"
+#os.environ["HTTPS_PROXY"] = "http://cache.esiee.fr:3128"
+#server = Server('ldap.esiee.fr', use_ssl=True, get_info=ALL)
+#conn = Connection(server, user="uid=giller,ou=Users,dc=esiee,dc=fr", password='plop')
 
-print(conn)
+#print(conn)
+#conn.open()
+#conn.bind()
+#print(conn.result)
+#conn.unbind()
+
+server = Server('ldap.esiee.fr', use_ssl=True, get_info=ALL)
+conn = Connection(server)
 conn.open()
-conn.bind()
-print(conn)
-conn.unbind()
+conn.search('dc=esiee, dc=fr', '(&(objectclass=person)(uid=bercherj))',
+        attributes=['sn', 'NumeroCCIP',  'idAurion', 'principalMail', 'googleMail',
+            'telephoneNumber',  'displayName', 'roomNumber', 'givenName',
+            'dateCreation', 'dateExpiration', 'annuairePresent', 'mailEDU', 'Name'])
+print(conn.entries[0])
