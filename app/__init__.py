587a6d3419bafc4f175682f7ba17668aeafc001a
Aire-One <erwan.maugere@gmail.com>
2016-10-19 09:38:11 +0200

    add mail configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
-from flask_sqlalchemy import SQLAlchemy
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models, forms, ldap
+mail = Mail(app)
+
+from app import views, models
+
--- config.py
+++ config.py
@@ -6,3 +6,14 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+# administrators emails list
+MAIL_ADMINS = []
+
