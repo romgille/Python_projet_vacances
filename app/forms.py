411f632ec8be4a89d3b8763c7985a59b72b5b674
Charlotte RAVILLY <ravillyc@pc5403u.esiee.fr>
2016-10-19 11:22:49 +0200

    ajout formulaire prise et depot vacances

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,7 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, InputRequired
+from wtforms.fields.html5 import DateField
 from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
@@ -8,3 +11,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
 
     remember_me = BooleanField('remember_me', default=False)
+
+class PriseForm(FlaskForm):
+	priseDateDebut = DateField('priseDateDebut', format='%d/%m/%Y')
+	priseDateFin = DateField('priseDateFin', format='%d/%m/%Y')
+	priseNbJours = IntegerField('priseNbJours',validators=[InputRequired()])
+
+class DepotForm(FlaskForm):
+	depotDateDebut = DateField('depotDateDebut', format='%d/%m/%Y')
+	depotDateFin = DateField('depotDateFin', format='%d/%m/%Y')
+	depotNbJours = IntegerField('depotNbJours', validators=[DataRequired()])
+
+
