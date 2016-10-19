d5a10adf77a14e2a9e663c75927df0ffa1958710
Aire-One <erwan.maugere@gmail.com>
2016-10-19 09:38:11 +0200

    add mail configs

--- config.py
+++ config.py
@@ -4,3 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+# administrators emails list
+MAIL_ADMINS = []
+
