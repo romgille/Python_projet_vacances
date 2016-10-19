78777badb57ddfc5a877162eac8f131c71dd7693
Romain Gille <romain.gille@gmail.com>
2016-10-19 09:44:27 +0200

    tests avec ldap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
+from ldap3 import Connection
+from ldap3 import Server, ALL
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+
 import os
 
 
@@ -14,6 +21,7 @@ mail = Mail(app)
 bootstrap = Bootstrap(app)
 
 from app import views, models
+
 
 os.environ["HTTP_PROXY"] = "http://cache.esiee.fr:3128"
 os.environ["HTTPS_PROXY"] = "http://cache.esiee.fr:3128"
