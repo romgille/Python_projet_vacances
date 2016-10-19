49f133fd6600ffa0bc2f37ad1f08f7200e0102e8
Victor <victor.houdiard@edu;esiee;fr>
2016-10-19 11:55:17 +0200

    Ajout de la page "Autorisations de vacances"

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 from app import app
 from app.forms import LoginForm
+from app import db,models
 
 
 @app.route('/')
@@ -25,3 +26,20 @@ def login():
     return render_template('login.html',
                            title='Sign In',
                            form=form)
+
+@app.route('/admission_vacances',methods=['GET','POST'])
+def admission_vacances():
+    u=models.User.query.filter_by(resp_id=1).all()
+    l=[]
+    v=[]
+    for j in u:
+        v=models.Vacances.query.filter_by(user_id=j.user_id,status=0).all()
+        for n in v:
+            l.append(n)
+    print (l[0].user_id)
+
+
+    return render_template('admission_vacances.html',
+                           title='Autorisations',
+                           l=l,
+                           models=models)
