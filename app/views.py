c0218f681788dfe51c0555f128177e5e41c04db0
romgille <Re@uM@in96>
2016-11-28 11:51:55 +0100

    corrections erreurs mineures

--- app/views.py
+++ app/views.py
@@ -1,7 +1,5 @@
-from flask import flash
 from flask import redirect
 from flask import render_template
-from flask import url_for
 
 from app import app
 from app.forms import LoginForm
@@ -25,8 +23,8 @@ def login():
         exists = db.session.query(db.session.query(User).filter_by(login=form.login.data).exists()).scalar()
         user_login = form.login.data
         if not exists:
-            user = User().create_user()
-            db.session.add(user)
+            actual_user = User().create_user()
+            db.session.add(actual_user)
             db.session.commit()
         return redirect('/user/' + user_login)
     return render_template('login.html',
@@ -41,8 +39,8 @@ def login():
 
 @app.route('/user/<user_login>')
 def user(user_login):
-    user = User.query.filter_by(login=user_login).first()
-    nom = user.nom
-    prenom = user.prenom
+    actual_user = User.query.filter_by(login=user_login).first()
+    nom = actual_user.nom
+    prenom = actual_user.prenom
     return render_template('user.html',
                            nom=prenom + ' ' + nom)
