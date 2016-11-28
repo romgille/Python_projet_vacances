8e1f58b0ae8c7c8c5c56a74d8c6e62f5e9537956
Mourouguesh COUNDJIDAPADAME <coundjim@pc5403t.esiee.fr>
2016-11-28 12:02:39 +0100

    changement de style

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_mail import Mail
 
+from flask_bootstrap import Bootstrap
+
+from flask_wtf import FlaskForm
+
+
+
 import os
 
 
@@ -15,6 +21,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import views, models
 
