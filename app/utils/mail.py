a5b833873d76c483d8eadd5c2d5688afe6e7352d
Aire-One <erwan.maugere@gmail.com>
2016-12-05 12:53:42 +0100

    mail notification package

--- app/utils/mail.py
+++ app/utils/mail.py
@@ -2,30 +2,69 @@ from flask import render_template
 from flask_mail import Message
 from app import mail, models
 
+from datetime import date
+from enum import Enum
 
-class Mail:
+
+class Mail :
+
     # send_mail
     # procedure to send mail
     #
-    # @param subject string 
-    # @param sender sender
-    # @param recipients [string] array of recipients
-    # @param text_body string 
-    #  @param html_body string
+    # @param subject string : mail subject
+    # @param recipients [string] : array of recipients
+    # @param text_body string : mail text content
+    # @param html_body string : mail html content
     @staticmethod
-    def send_mail(subject, recipients, text_body, html_body):
-        message = Message(subject, recipients=recipients)
+    def send_mail (subject, recipients, text_body, html_body) :
+        message = Message (subject, recipients = recipients)
         message.body = text_body
         message.html = html_body
 
         mail.send(message)
 
+
+    # ENUM TYPE notification_type
+    class notification_type (Enum) :
+        add_vacation        = 1,
+        remove_vacation     = 2
+
+
+    # vacation_notification
+    # procedure to send a mail notification 
+    #
+    # @param user Model.User : user 
+    # @param dates_start datetime.date : date of start
+    # @param dates_end datetime.date : date of end
+    # @param notificationType notification_type : type of mail to send
     @staticmethod
-    def add_vacation_notification(user, dates):
+    def vacation_notification (user, dates, notificationType) :
         responsable = models.User.query.get(user.resp_id)
-        user.send_mail("Prise de congée de " + user.nom + " " + user.prenom,
-                       [responsable.email],
-                       render_template("app/templates/mail/addVacation.txt", user=user, responsable=responsable,
-                                       dates=dates),
-                       "")  # render_template ("app/templates/mail/addVacation.html", user=user,
-        # responsable=responsable, dates=dates))
+        mail_object = "[AUTO] [app/vacances]"
+        template_base_name = "app/templates/mails/"
+        
+        if notificationType == notification_type.add_vacation :
+            mail_object = mail_object + "Prise de congée de " + user.nom + " " + user.prenom
+            template_base_name = template_base_name + "add_vacation"
+        else if notificationType == notification_type.remove_vacation :
+            mail_object = mail_object + "Retrait de congée de " + user.nom + " " + user.prenom
+            template_base_name = template_base_name + "remove_vacation"
+            
+        user.send_mail (
+            mail_object,
+            [responsable.email],
+            render_template (
+                template_base_name + ".txt",
+                user = user,
+                responsable = responsable,
+                dates = [ date_start, date_end ]),
+            render_template (
+                template_base_name + ".html",
+                user = user,
+                responsable = responsable, 
+                dates = [ date_start, date_end ]))
+                
+                
+                
+                
+                
