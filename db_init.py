1eb0e6da65db8f340ba80b7ac97ea78e34d664ef
romgille <Re@uM@in96>
2016-12-12 22:52:05 +0100

    initialisation de la base

--- db_init.py
+++ db_init.py
@@ -1,50 +1,15 @@
 #!flask/bin/python
+from app import db
+from app.models import User
 
-import csv
-import re
+u = User(user_id=1, login='redisj', nom='REDIS', prenom='Jean', email='jean.redis@esiee.fr', resp_id=1, role=1)
+u1 = User(user_id=2, login='bertrang', nom='BERTRAND', prenom='Gilles', email='gilles.bertrand@esiee.fr', resp_id=2, role=1)
+u2 = User(user_id=3, login='baudoing', nom='BAUDOIN', prenom='Geneviève', email='genevieve.baudoin@esiee.fr', resp_id=3, role=1)
+u3 = User(user_id=4, login='lissorgg', nom='LISSORGUES', prenom='Gaelle', email='jgaelle.lissorgues@esiee.fr', resp_id=4, role=1)
 
-from app import db, models
-from app.ldap import Ldap
-
-
-dept_resp = {}
-with open ('csv/resp_depts.csv', 'r') as csvfile :
-    reader = csv.reader (csvfile, delimiter=',')
-    for row in reader :
-        if row[0] == 'dept' :
-            dept = row[1].strip()
-            name = row[3].strip()
-            firstname =  row[2].strip()
-            #email = row[4].strip()
-            login = re.sub('[éèê]', 'e', (name[:8] + firstname[0]).lower()) # TODO : check more special chars
-            user_data = Ldap.createUserFromLoginWithoutPass(login)
-            user = models.User(
-                login=login,
-                nom=user_data[0],
-                prenom=user_data[1],
-                email=user_data[2],
-                resp_id=0,
-                role=0)
-            db.session.add(user)
-            dept_resp[dept] = user
-
-with open ('csv/liste_enseignants_2016-17.csv', 'r') as csvfile :
-    reader = csv.reader (csvfile, delimiter=',')
-    for row in reader :
-        name = re.search ('([A-Z\']|( [A-Z]{2,}))+', row[0]).group(0).strip()
-        firstname = row[0].split (name)[1].strip()
-        login = re.sub('[éèê]', 'e', (name[:8] + firstname[0]).lower()) # TODO : check more special chars
-        resp = dept_resp[ row[1].split ('EP : Dept. ')[1] ]
-        user_data = Ldap.createUserFromLoginWithoutPass(login)
-        user = models.User(
-            login=login,
-            nom=user_data[0],
-            prenom=user_data[1],
-            email=user_data[2],
-            resp_id=resp.user_id,
-            role=0)
-        db.session.add(user)
+db.session.add(u)
+db.session.add(u1)
+db.session.add(u2)
+db.session.add(u3)
 
 db.session.commit()
-
-
